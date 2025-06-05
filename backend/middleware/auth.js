3116a75b9a22a8a8fc7325cf41bd7ad707cf2908
loki12ne <158790218+loki12ne@users.noreply.github.com>
2025-06-05 13:14:46 +0700

    Delete backend directory

--- backend/middleware/auth.js
+++
@@ -1,6 +0,0 @@
-const isAuthenticated = (req, res, next) => {
-  if (req.session.user) return next();
-  res.status(401).json({ error: 'Please log in first' });
-};
-
-module.exports = { isAuthenticated };
